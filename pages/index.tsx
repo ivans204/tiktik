3c238faf013205911e6895e69c60dcccfca27df7
ivans204 <ivansvrga204@gmail.com>
2022-09-17 01:04:31 +0200

    show videos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,38 @@
 import type { NextPage } from 'next';
+import axios from 'axios';
+import { Video } from '../types';
+import VideoCard from '../components/VideoCard';
+import NoResults from '../components/NoResults';
 
-const Home: NextPage = () => {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+interface IProps {
+    videos: Video[];
+}
+
+const Home = ({ videos }: IProps) => {
+    console.log(videos);
+    return (
+        <div className="flex flex-col gap-10 videos h-full">
+            {videos.length ? (
+                videos?.map((video: Video) => (
+                    <VideoCard post={video} isShowingOnHome key={video._id} />
+                ))
+            ) : (
+                <NoResults text={`No Videos`} />
+            )}
+        </div>
+    );
+};
+
+export const getServerSideProps = async () => {
+    const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+    console.log(data);
+
+    return {
+        props: {
+            videos: data,
+        },
+    };
 };
 
 export default Home;
